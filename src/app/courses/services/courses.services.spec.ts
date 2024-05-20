56959289e5fd7d27881154eae4e5d1907b8c0e43
Jesus Campo Alvarez <eduardocampo91@gmail.com>
2024-05-19 23:51:43 -0500

    unitesting the method to find course by id, made sure only one request is sent and its not affecting other test

--- src/app/courses/services/courses.services.spec.ts
+++ src/app/courses/services/courses.services.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from "@angular/core/testing";
 import { CoursesService } from "./courses.service";
 import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
+import { COURSES } from "../../../../server/db-data";
 
 describe("CourseService", () => {
     let coursesServices: CoursesService;
     let httpTestingController: HttpTestingController;
-    
+
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [HttpClientTestingModule],
@@ -21,6 +22,40 @@ describe("CourseService", () => {
     });
 
     it('should retrieve all courses', () => {
+        coursesServices.findAllCourses()
+            .subscribe(courses => {
+                expect(courses).toBeTruthy('No courses were finded');
+                expect(courses.length).toBe(12, 'Incorrect number of courses');
+
+                const course = courses.find(course => course.id == 12);
+                expect(course.titles.description).toBe("Angular Testing Course")
+            });
         
+        //with the testing controller we can validate some extra information about the recieved data
+        const req = httpTestingController.expectOne('/api/courses');
+        expect(req.request.method).toEqual("GET");
+        req.flush({payload: Object.values(COURSES)});
+
+        //we should verify that ony the http request made in the method epectOne are being made
+        //httpTestingController.verify();
+    });
+
+    it('should find a course by its id', () => {
+        coursesServices.findCourseById(12)
+            .subscribe(courses => {
+                expect(courses).toBeTruthy('No courses with that id were finded');
+                expect(courses.id).toBe(12, 'Incorrect number of courses');
+            });
+        
+        //with the testing controller we can validate some extra information about the recieved data
+        const req = httpTestingController.expectOne('/api/courses/12');
+        expect(req.request.method).toEqual("GET");
+        req.flush(COURSES[12]);
+
+        //httpTestingController.verify();
+    });
+
+    afterEach(() => {
+        httpTestingController.verify();
     });
 });
